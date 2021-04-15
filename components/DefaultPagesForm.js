d67c905858cf90e7b07669e8baa5f0c1158d3dbb
ghettojezuz <rebim@yandex.ru>
2021-04-15 16:48:05 +0700

    Finished admin main page

--- components/DefaultPagesForm.js
+++ components/DefaultPagesForm.js
@@ -1,10 +1,147 @@
 import FormWrapper from "./FormWrapper";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import * as yup from "yup";
+import {useState} from "react";
+import {Formik} from "formik";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
+import {TextField} from "@material-ui/core";
+import MenuItem from "@material-ui/core/MenuItem";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+        width: '100%',
+    },
+    mt16: {
+        marginTop: '16px'
+    },
+    mt20: {
+        marginTop: '20px'
+    }
+}));
+
+const validationSchema = yup.object({
+    forDriver: yup
+        .string()
+        .required('Это поле обязательно'),
+    forMechanic: yup
+        .string()
+        .required('Это поле обязательно'),
+    forAdmin: yup
+        .string()
+        .required('Это поле обязательно'),
+});
 
 export default function DefaultPagesForm({}) {
+    const classes = useStyles();
+    const pages = [
+        {
+            id: 1,
+            page: 'Мои путевые листы',
+            value: '/pl'
+        },
+        {
+            id: 2,
+            page: 'Реестр путевых листов',
+            value: '/reg'
+        },
+        {
+            id: 3,
+            page: 'Панель администратора',
+            value: '/admin'
+        },
+    ];
+
+    const [initialValues, setInitialValues] = useState({
+        forDriver: "",
+        forMechanic: "",
+        forAdmin: "",
+    });
+
+    const handleSubmit = (values) => {
+        console.log(values)
+    };
 
     return (
-        <FormWrapper padding="32px 20px">
+        <Formik onSubmit={(values) => {
+            handleSubmit(values);
+        }}
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                enableReinitialize={true}>
 
-        </FormWrapper>
+            {(formikProps) => (
+                <FormWrapper padding="32px 20px">
+                    <form onSubmit={formikProps.handleSubmit}>
+                        <FormLabel component="legend" disabled>Страницы ролей по умолчанию</FormLabel>
+
+                        <FormControl component="fieldset" className={classes.formControl}>
+                            <TextField
+                                select
+                                id="forDriver"
+                                label="Для водителя"
+                                variant="outlined"
+                                className={classes.mt16}
+                                value={formikProps.values.forDriver}
+                                onChange={formikProps.handleChange('forDriver')}
+                                helperText={formikProps.touched.forDriver ? formikProps.errors.forDriver : ""}
+                                error={formikProps.touched.forDriver && Boolean(formikProps.errors.forDriver)}
+                            >
+                                {pages.map((page) => (
+                                    <MenuItem key={page.id} value={page.value}>{page.page}</MenuItem>
+                                ))}
+                            </TextField>
+                        </FormControl>
+
+                        <FormControl component="fieldset" className={classes.formControl}>
+                            <TextField
+                                select
+                                id="forMechanic"
+                                label="Для механика"
+                                variant="outlined"
+                                className={classes.mt16}
+                                value={formikProps.values.forMechanic}
+                                onChange={formikProps.handleChange('forMechanic')}
+                                helperText={formikProps.touched.forMechanic ? formikProps.errors.forMechanic : ""}
+                                error={formikProps.touched.forMechanic && Boolean(formikProps.errors.forMechanic)}
+                            >
+                                {pages.map((page) => (
+                                    <MenuItem key={page.id} value={page.value}>{page.page}</MenuItem>
+                                ))}
+                            </TextField>
+                        </FormControl>
+
+                        <FormControl component="fieldset" className={classes.formControl}>
+                            <TextField
+                                select
+                                id="forAdmin"
+                                label="Для администратора"
+                                variant="outlined"
+                                className={classes.mt16}
+                                value={formikProps.values.forAdmin}
+                                onChange={formikProps.handleChange('forAdmin')}
+                                helperText={formikProps.touched.forAdmin ? formikProps.errors.forAdmin : ""}
+                                error={formikProps.touched.forAdmin && Boolean(formikProps.errors.forAdmin)}
+                            >
+                                {pages.map((page) => (
+                                    <MenuItem key={page.id} value={page.value}>{page.page}</MenuItem>
+                                ))}
+                            </TextField>
+                        </FormControl>
+
+                        <Button variant="contained"
+                                color="primary"
+                                disableElevation
+                                type='submit'
+                                fullWidth
+                                className={classes.mt20}
+                        >
+                            Сохранить
+                        </Button>
+                    </form>
+                </FormWrapper>
+            )}
+        </Formik>
     )
 }
--- components/DirsRolesForm.js
+++ components/DirsRolesForm.js
@@ -1,10 +1,164 @@
 import FormWrapper from "./FormWrapper";
+import FormLabel from "@material-ui/core/FormLabel";
+import FormControl from "@material-ui/core/FormControl";
+import MenuItem from "@material-ui/core/MenuItem";
+import * as yup from "yup";
+import {useState} from "react";
+import {Formik} from "formik";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import Button from "@material-ui/core/Button";
+import Select from "@material-ui/core/Select";
+import InputLabel from '@material-ui/core/InputLabel';
+import Chip from "@material-ui/core/Chip";
+import FormHelperText from "@material-ui/core/FormHelperText";
+
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+        width: '100%',
+    },
+    mt16: {
+        marginTop: '16px'
+    },
+    mt20: {
+        marginTop: '20px'
+    }
+}));
+
+const validationSchema = yup.object({
+    driversDir: yup
+        .array()
+        .min(1, 'Выберите минимум одну роль'),
+    carsDir: yup
+        .array()
+        .min(1, 'Выберите минимум одну роль'),
+});
 
 export default function DirsRolesForm({}) {
+    const classes = useStyles();
+    const roles = [
+        {
+            id: 1,
+            role: "Водитель",
+            value: "driver"
+        },
+        {
+            id: 2,
+            role: "Механик",
+            value: "mechanic"
+        },
+        {
+            id: 3,
+            role: "Администратор",
+            value: "admin"
+        },
+
+    ];
+
+    const [initialValues, setInitialValues] = useState({
+        driversDir: [],
+        carsDir: [],
+    });
+
+    const getMultipleRenderValue = (selected) => {
+        let result = [];
+
+        for (let i = 0; i < roles.length; i++) {
+            for (let j = 0; j < selected.length; j++) {
+                if (roles[i].value === selected[j]) {
+                    result.push(roles[i].role)                }
+            }
+        }
+
+        return (
+            <div className="disableScrollbar">
+                {result.map((item) => (
+                    <Chip key={item} label={item} style={{marginRight: "6px"}}/>
+                ))}
+            </div>
+        );
+    };
+
+    const handleSubmit = (values) => {
+        console.log(values)
+    };
 
     return (
-        <FormWrapper padding="32px 20px">
+        <Formik onSubmit={(values) => {
+            handleSubmit(values);
+        }}
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                enableReinitialize={true}>
 
-        </FormWrapper>
+            {(formikProps) => (
+                <FormWrapper padding="32px 20px">
+                    <form onSubmit={formikProps.handleSubmit}>
+                        <FormLabel component="legend" disabled>Права на редактирование справочников</FormLabel>
+
+                        <FormControl variant="outlined"
+                                     fullWidth
+                                     className={classes.mt16}
+                                     error={formikProps.touched.driversDir && Boolean(formikProps.errors.driversDir)}
+                        >
+                            <InputLabel id="driversDir_label" style={{padding: "0 4px", backgroundColor: "#ffffff"}}>
+                                Справочник водителей
+                            </InputLabel>
+                            <Select
+                                id="driversDir"
+                                labelId="driversDir_label"
+                                multiple
+                                fullWidth
+                                value={formikProps.values.driversDir}
+                                onChange={(e) => {
+                                    formikProps.setFieldValue('driversDir', e.target.value);
+                                }}
+                                renderValue={(selected) => getMultipleRenderValue(selected)}
+                            >
+                                {roles.map((role) => (
+                                    <MenuItem key={role.id} value={role.value}>{role.role}</MenuItem>
+                                ))}
+                            </Select>
+                            <FormHelperText>{formikProps.touched.driversDir ? formikProps.errors.driversDir : ""}</FormHelperText>
+                        </FormControl>
+
+                        <FormControl variant="outlined"
+                                     fullWidth
+                                     className={classes.mt16}
+                                     error={formikProps.touched.carsDir && Boolean(formikProps.errors.carsDir)}
+                        >
+                            <InputLabel id="carsDir_label" style={{padding: "0 4px", backgroundColor: "#ffffff"}}>
+                                Справочник автомобилей
+                            </InputLabel>
+                            <Select
+                                id="carsDir"
+                                labelId="carsDir_label"
+                                multiple
+                                fullWidth
+                                value={formikProps.values.carsDir}
+                                onChange={(e) => {
+                                    formikProps.setFieldValue('carsDir', e.target.value);
+                                }}
+                                renderValue={(selected) => getMultipleRenderValue(selected)}
+                            >
+                                {roles.map((role) => (
+                                    <MenuItem key={role.id} value={role.value}>{role.role}</MenuItem>
+                                ))}
+                            </Select>
+                            <FormHelperText>{formikProps.touched.carsDir ? formikProps.errors.carsDir : ""}</FormHelperText>
+                        </FormControl>
+
+                        <Button variant="contained"
+                                color="primary"
+                                disableElevation
+                                type='submit'
+                                fullWidth
+                                className={classes.mt20}
+                        >
+                            Сохранить
+                        </Button>
+                    </form>
+                </FormWrapper>
+            )}
+        </Formik>
     )
 }
